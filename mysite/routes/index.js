c9286e8ac39ee920894d5137d018494a94e06f47
Hoon <loslch@gmail.com>
2012-04-06 22:20:50 +0900

    adjust swig template.

--- mysite/routes/index.js
+++ mysite/routes/index.js
@@ -4,5 +4,22 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', {});
+};
+
+/*
+ * GET people.
+ */
+ people = [
+    { name: 'Paul', age: 28 },
+    { name: 'Jane', age: 26 },
+    { name: 'Jimmy', age: 45 }
+];
+
+exports.people = function(req, res){
+    res.render('people', { people: people });
+};
+
+exports.person = function(req, res){
+    res.render('person', { person: people[req.params.id-1] });
 };
--- mysite/server.js
+++ mysite/server.js
@@ -5,18 +5,29 @@
 
 var express = require('express')
   , routes = require('./routes');
-var port = process.env.port || 3000;
+
+var swig = require('swig');
 var app = module.exports = express.createServer();
 
+var port = process.env.port || 3000;
 // Configuration
 
+swig.init({ 
+  root: __dirname + '/views',
+  allowErrors: true,
+  encoding: 'utf8'
+});
+
 app.configure(function(){
+  app.register('.html', swig);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
   app.use(express.bodyParser());
+  app.set('view cache', true);
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.set('view options', { layout: false });
 });
 
 app.configure('development', function(){
@@ -30,6 +41,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/people', routes.people);
+app.get('/people/:id', routes.person);
 
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
